929efc5d9d427f869a92ab6a987dd1afd16590fa
Dorian Janninelle <dojannin@c1r5p2.42nice.fr>
2024-06-05 19:36:48 +0200

    JsprEncoreEtreSurMonRepos

---
+++ save/test/backend/backend/static/js/router.js
@@ -0,0 +1,425 @@
+//SPA Request GET, load page /user/...
+function loadPageUsers(pagePath, pushState = true) {
+    $.ajax({
+        url: `/users/${pagePath}/`,
+        success: function(response) {
+            $('#app-content').html(response.html);
+            if (pushState) {
+                history.pushState({ path: pagePath, content: response.html }, '', `/users/${pagePath}`);
+            }
+            bindFormEvent();
+        },
+        error: function(error) {
+            console.error('Error loading the page:', error);
+        }
+    });
+}
+
+//SPA Request GET, load page /matchmaking/...
+function loadPageMatchmaking(pagePath, pushState = true) {
+    $.ajax({
+        url: `/matchmaking/${pagePath}/`,
+        success: function(response) {
+            $('#app-content').html(response.html);
+            if (pushState) {
+                 history.pushState({ path: pagePath, content: response.html }, '', `/matchmaking/${pagePath}`);
+            }
+            bindFormEvent();
+            // if (typeof initializeGame === 'function') {
+            //     initializeGame();
+            // }
+              // Fetch opponent name before initializing the game
+              $.ajax({
+                type: 'GET',
+                url: '/games/get_opponent_name/',  // L'URL de la vue créée pour obtenir le nom de l'adversaire
+                success: function(response) {
+                    if (response.status === 'success') {
+                        localStorage.setItem('opponent_name', response.opponent_name);
+                        const player1Name = localStorage.getItem('username');
+                        const player2Name = localStorage.getItem('opponent_name');
+                        console.log('Player 1 Name:', player1Name);
+                        console.log('Player 2 Name:', player2Name);
+
+                        // Initialiser le jeu après avoir récupéré le nom de l'adversaire
+                        if (typeof initializeGame === 'function') {
+                            initializeGame();
+                        }
+                    } else {
+                        console.error('Error fetching opponent name:', response.message);
+                    }
+                },
+                error: function(xhr, status, error) {
+                    console.error('Error fetching opponent name:', error);
+                }
+            });
+        },
+        error: function(error) {
+            console.error('Error loading the page:', error);
+        }
+    });
+}
+
+//SPA Request GET, load page /user/...
+function loadPageGames(pagePath, pushState = true) {
+    $.ajax({
+        url: `/games/${pagePath}/`,
+        success: function(response) {
+            $('#app-content').html(response.html);
+            if (pushState) {
+                history.pushState({ path: pagePath, content: response.html }, '', `/games/${pagePath}`);
+            }
+            bindFormEvent();
+            if (typeof initializeGame === 'function' && pagePath === 'play') {
+                initializeGame();
+            }
+        },
+        error: function(error) {
+            console.error('Error loading the page:', error);
+        }
+    });
+}
+
+//SPA Request GET, load Setting page
+function loadPageUsersSettings(pagePath, pushState = true) {
+    event.preventDefault();
+    var profileData = $('#profileData');
+
+    if (profileData.is(':visible')) {
+        profileData.hide();
+    } else {
+        $.ajax({
+            url: `/users/${pagePath}/`,
+            success: function(response) {
+                $('#profileData').html(response.html);
+                $('#profileData').show();
+                bindFormEvent(); 
+            },
+            error: function(error) {
+                console.error('Error loading the page:', error);
+            }
+        });
+    }
+}
+
+//SPA Request GET, load page /Blockchain/...
+function loadPageBlockchain(pagePath, pushState = true) {
+    $.ajax({
+        url: `/blockchain/${pagePath}/`,
+        success: function(response) {
+            $('#app-content').html(response.html);
+            if (pushState) {
+                history.pushState({ path: pagePath, content: response.html }, '', `/blockchain/${pagePath}`);
+            }
+            bindFormEvent(); 
+        },
+        error: function(error) {
+            console.error('Error loading the page:', error);
+        }
+    });
+}
+
+//SPA Request GET, load page /Aouth/...
+function loadPageAouth(pagePath, pushState = true) {
+    $.ajax({
+        url: `/aouth/${pagePath}/`,
+        success: function(response) {
+            $('#app-content').html(response.html);
+            if (pushState) {
+                history.pushState({ path: pagePath, content: response.html }, '', `/${pagePath}`);
+            }
+            bindFormEvent(); 
+        },
+        error: function(error) {
+            console.error('Error loading the page:', error);
+        }
+    });
+}
+
+//SPA Request GET, load page profile of a friend /user/...
+function loadPageFriendProfile(username, pushState = true) {
+    $.ajax({
+        url: `/users/friend-profile/${username}/`,
+        success: function(response) {
+            $('#app-content').html(response.html);
+            if (pushState) {
+                history.pushState({ path: `/users/friend-profile/${username}`, content: response.html }, '', `/users/friend-profile/${username}`);
+            }
+            bindFormEvent(); 
+        },
+        error: function(error) {
+            console.error('Error loading the page:', error);
+        }
+    });
+}
+
+//SPA Request POST, Form register, login, 2FA
+function bindFormEvent() {
+    //REgister
+    $('#registerForm').off('submit').on('submit', function(e) {
+        e.preventDefault();
+        const formData = new FormData(this);
+
+        $.ajax({
+            type: 'POST',
+            url: this.action,
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                if (response.status === 'success') {
+                    loadPageAouth(response.redirectUrl.replace(/^\//, ''), true);
+                } else {
+                    console.log('Success with unexpected status:', response.message);
+                    loadPageAouth(response.redirectUrl.replace(/^\//, ''), true);
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error("Error submitting form:", error);
+                
+            }
+        });
+    });
+
+    //TwoFactor'twoFactorAouth"
+    $('#twoFactorAouth').off('submit').on('submit', function(e) {
+        e.preventDefault();
+        const formData = new FormData(this);
+        
+        $.ajax({
+            type: 'POST',
+            url: this.action,
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                if (response.status === 'success') {
+                    // locasStorage.setItem('playername', response.username);
+                    localStorage.setItem('access_token', response.access_token);
+                    localStorage.setItem('refresh_token', response.refresh_token);
+                    localStorage.setItem('csrf_token', response.csrf_token);
+                    loadPageUsers(response.redirectUrl.replace(/^\//, ''), true);
+                } else {
+                    console.log('Success with unexpected status:', response.message);
+                    loadPageAouth(response.redirectUrl.replace(/^\//, ''), true);
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error("Error submitting form:", error);
+            }
+        });
+    });
+
+    $('#Set_image').off('submit').on('submit', function(e) {
+        e.preventDefault();
+        const formData = new FormData(this);
+
+        $.ajax({
+            type: 'POST',
+            url: this.action,
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                console.log("Success image changed");
+               
+            },
+            error: function(xhr, status, error) {
+                console.error("Error submitting form:", error);
+            }
+        });
+    });
+
+    $('#Set_username').off('submit').on('submit', function(e) {
+        e.preventDefault();
+        const formData = new FormData(this);
+
+        $.ajax({
+            type: 'POST',
+            url: this.action,
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                console.log("Success username changed");
+               
+            },
+            error: function(xhr, status, error) {
+                console.error("Error submitting form:", error);
+            }
+        });
+    });
+    
+
+    // Setting change the Password
+    $('#Set_password').off('submit').on('submit', function(e) {
+        e.preventDefault();
+        const formData = new FormData(this);
+
+        $.ajax({
+            type: 'POST',
+            url: this.action,
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                console.log("Success password changed");
+               
+            },
+            error: function(xhr, status, error) {
+                console.error("Error submitting form:", error);
+            }
+        });
+    });
+
+    // Setting change the 2factor
+    $('#2faForm').off('submit').on('submit', function(e) {
+        e.preventDefault();
+        const formData = new FormData(this);
+
+        $.ajax({
+            type: 'POST',
+            url: this.action,
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                console.log("Success 2faForm");
+               
+            },
+            error: function(xhr, status, error) {
+                console.error("Error submitting form:", error);
+            }
+        });
+    });
+
+    $('#enable_2fa').off('change').on('change', function(e) {
+        e.preventDefault();
+        bindFormEvent()
+        $('#2faForm').submit();
+        
+    });
+
+    //Auth login 
+    $('#loginForm').off('submit').on('submit', function(e) {
+        e.preventDefault();
+        const formData = new FormData(this);
+
+        $.ajax({
+            type: 'POST',
+            url: this.action,
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                console.log("Response received:", response);
+                if (response.redirectUrl == 'home') {
+                    console.log("Response received:", response);
+                    localStorage.setItem('username', response.username)
+                    localStorage.setItem('access_token', response.access_token);
+                    localStorage.setItem('refresh_token', response.refresh_token);
+                    localStorage.setItem('csrf_token', response.csrf_token);
+                    loadPageUsers(response.redirectUrl.replace(/^\//, ''), true);
+                }
+                else if (response.status === 'success') {
+                    loadPageAouth(response.redirectUrl.replace(/^\//, ''), true);
+                } else {
+                    console.log('Success with unexpected status:', response.message);
+                    loadPageAouth(response.redirectUrl.replace(/^\//, ''), true);
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error("Error submitting form:", error);
+            }
+        });
+    });
+
+    $('#searchFriends').off('submit').on('submit', function(e) {
+        e.preventDefault();
+        const queryData = $(this).serialize();
+        
+        $.ajax({
+            type: 'GET',
+            url: this.action,
+            data: queryData,
+            success: function(response) {
+                $('#app-content').html(response.html);
+                history.pushState({ path: '', content: response.html }, '', window.location.pathname);
+                bindFormEvent();
+            },
+            error: function(xhr, status, error) {
+                console.error("Error submitting form:", error);
+            }
+        });
+    });
+    
+    $('#AddFriends').off('submit').on('submit', function(e) {
+        e.preventDefault();
+        const formData = new FormData(this);
+
+        $.ajax({
+            type: 'POST',
+            url: this.action,
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                loadPageUsers('friend', true);
+                bindFormEvent();
+            },
+            error: function(xhr, status, error) {
+                console.error("Error submitting form:", error);
+            }
+        });
+    });
+}
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        
+        reader.onload = function(e) {
+            $('#imagePreview').attr('src', e.target.result);
+        }
+        
+        reader.readAsDataURL(input.files[0]); // convert to base64 string
+    }
+}
+
+$(document).ready(function() {
+    bindFormEvent();
+
+    $("#file-upload").change(function() {
+        readURL(this);
+    });
+
+    $(document).on('pjax:end', function() {
+        bindFormEvent();
+    });
+
+    history.replaceState({ path: window.location.pathname, content: $('#app-content').html() }, '', window.location.pathname);
+
+    $('body').on('click', '#goBackButton', function()
+    {
+        window.history.back();
+    });
+
+    window.addEventListener('popstate', function(event) {
+        if (window.location.pathname === '/aouth/twofactor/') {
+            loadPageAouth('aouth_logout'); 
+        }
+        else if (event.state) {
+            $('#app-content').html(event.state.content);
+            bindFormEvent();
+        }
+    });
+});
+
+
+function setupGoBackButton() {
+    $('#goBackButton').on('click', function() {
+        window.history.back();
+    });
+}
+
+
+
---
+++ save/test/backend/games/views/games.py
@@ -0,0 +1,85 @@
+import logging
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.http import JsonResponse
+from users.models import User
+from games.models import Game 
+from aouth.views.jwt import jwt_login_required
+from users.views.users import *
+
+logger = logging.getLogger(__name__)
+
+# Middleware
+# ----------
+
+@jwt_login_required
+def game_init(request, player1_id, player2_id):
+    try:
+        player1 = User.objects.get(pk=player1_id.id)
+        player2 = User.objects.get(pk=player2_id.id)
+        new_game = Game.objects.create(
+            player1=player1,
+            player2=player2,
+            player1_score=0,
+            player2_score=0,
+            winner_id=None
+        )
+        new_game.save()
+        user_update_status(request, player1, "ingame")
+        user_add_to_match_history(request, player1, new_game)
+        user_update_status(request, player2, "ingame")
+        user_add_to_match_history(request, player2, new_game)
+        user_add_current_game(request, player1, new_game)
+        user_add_current_game(request, player2, new_game)
+        return new_game
+    except Exception as e:
+        logger.error(f"game_init error: {e}")
+        return None
+
+@jwt_login_required
+def game_update(request, game, player1_score, player2_score):
+    try:
+        game.player1_score = player1_score
+        game.player2_score = player2_score
+        if player1_score > player2_score:
+            game.winner = game.player1
+        elif player1_score < player2_score:
+            game.winner = game.player2
+        elif player2_score == -1:
+            game.winner = game.player2
+        elif player1_score == -1:
+            game.winner = game.player2
+        elif player1_score == player2_score:
+            game.draw = 1
+        game.save()
+        # ELO
+        user_update_status(request, game.player1, "online")
+        user_update_status(request, game.player2, "online")
+        user_remove_current_game(request, game.player1)
+        user_remove_current_game(request, game.player2)
+        return game
+    except Exception as e:
+        logger.error(f"game_update error: {e}")
+        return None
+
+@jwt_login_required
+def check_status_user(request):
+    logger.info(f"request: {request.user.status}")
+    context = request.user.status
+    return JsonResponse({'context': context})
+
+
+@jwt_login_required
+def get_opponent_name(request):
+    try:
+        current_game = request.user.current_game
+        if current_game:
+            if current_game.player1 == request.user:
+                opponent_name = current_game.player2.username
+            else:
+                opponent_name = current_game.player1.username
+            return JsonResponse({'status': 'success', 'opponent_name': opponent_name})
+        else:
+            return JsonResponse({'status': 'error', 'message': 'No current game found'})
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': str(e)})
